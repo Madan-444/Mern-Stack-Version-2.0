3e5cf88472217e2f1d08cf336aec429f7923b906
Madan-444 <mmadan3600@gmail.com>
2020-10-08 10:41:34 +0545

    simple restApi

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,56 @@
+//Importing middleswares
+
 const express = require('express');
 const http = require('http')
 const morgan = require('morgan')
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost'
 const port = 3300;
 
+// using middlewares 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname+ '/public'))
+app.all('/dishes',(req,res,next)=> {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+
+app.get('/dishes',(req,res,next)=> {
+    res.end('Will send all the dishes to you.')
+})
+app.post('/dishes',(req,res,next)=> {
+    res.end('Will add the dish: '+ req.body.name +' with details ' + req.body.description );
+})
+app.put ('/dishes',(req,res,next)=> {
+    res.sendStatus = 403;
+    res.end('PUT operation not supported in /dishes ');
+})
+app.delete('/dishes',(req,res,next)=> {
+    res.end('Deleting all the dishes !' );
+})
+
+// for dishId endpooint
+app.get('/dishes/:dishId',(req,res,next)=> {
+    res.end('Will send all the details of dishe:'+ req.params.dishId+' to you')
+})
+app.post('/dishes/:dishId',(req,res,next)=> {
+    res.sendStatus = 403;
+    res.end('POST operation not supported in /dishes/ '+ req.params.dishId);
+})
+app.put ('/dishes/:dishId',(req,res,next)=> {
+    res.write('Updating the dish:'+ req.params.dishId+'\n ')
+    res.end('Will update the dish: '+req.body.name+ ' with details '+ req.body.description);
+})
+app.delete('/dishes/:dishId',(req,res,next)=> {
+    res.end('Deleting dishe: '+ req.params.dishId );
+})
+
+
+app.use(express.static(__dirname+ '/public'));
 
 app.use((req,res,next)=> {
     res.statuscode = 200;
